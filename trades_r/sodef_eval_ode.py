e9ec4b6bef3efd2182834896c4523b8eb057dafe
Kohsin <102904030+Kohsin@users.noreply.github.com>
2022-08-16 10:49:51 +0800

    Update sodef_eval_ode.py

--- trades_r/sodef_eval_ode.py
+++ trades_r/sodef_eval_ode.py
@@ -3,7 +3,7 @@ import copy
 import logging
 import os
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID" 
-os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 import time
 import math
 import numpy as np
